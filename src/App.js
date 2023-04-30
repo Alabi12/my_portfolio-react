b508a3930b77e382db31c4496b5c164fbc489170
Alabi12 <wolo22001@gmail.com>
2023-04-30 22:40:04 +0000

    update the App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import NavBar from "./components/navbar/NavBar";
 import About from "./pages/abouts/Abouts";
+import Contacts from "./pages/contacts/Contacts";
 import Main from "./pages/main/Main";
 import Projects from "./pages/projects/Projects";
 
@@ -11,6 +12,7 @@ function App() {
       <Main />
       <Projects />
       <About />
+      <Contacts />
     </div>
   );
 }
