541011ee4c1ae03821b3884816cc7885cfc085bd
Alabi12 <wolo22001@gmail.com>
2023-05-01 14:56:36 +0000

    update the home and the styling

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import './home.css';
+import React from "react";
+import "./home.css";
 
 function Home() {
   return (
-    <div className="splash-container">
-      <h1>Welcome to my portfolio</h1>
-      <p>Here's where I showcase my latest projects and skills</p>
-      <button>View Portfolio</button>
-    </div>
+    <>
+      <div className="splash-container">
+        <h1>Welcome to my portfolio</h1>
+        <p>Here's where I showcase my latest projects and skills</p>
+      </div>
+    </>
   );
 }
 
