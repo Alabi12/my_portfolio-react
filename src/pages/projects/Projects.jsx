946eb5ae8e26fe8660de26aebcc9fbc95ec36f96
Alabi12 <wolo22001@gmail.com>
2023-05-03 22:48:42 +0000

    Add project images and set the links

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -4,19 +4,37 @@ import data from "./data";
 import "./projects.css";
 import SocialIcons from "../../components/socialIcons/SocialIcons";
 
+const handleClick = () => {
+  window.open("https://github.com/Alabi12/Budget-App.git");
+};
+
+const demoHandleClick = () => {
+  window.open("https://budget-app-m3yc.onrender.com");
+};
+
 function Projects() {
   return (
     <div className="wrapper">
       {data.map((data) => (
         <Card key={data.id} className="content">
-          <Card.Img variant="top" src={data.image} />
+          <Card.Img variant="top" className="Image" src={data.image} />
           <Card.Body>
+            <Card.Text className="heading">{data.name}</Card.Text>
             <Card.Text className="text">{data.description}</Card.Text>
+            <div className="slacks">
+              <div className="sl">{data.ruby}</div>
+              <div className="sl">{data.react}</div>
+              <div className="sl">{data.rails}</div>
+            </div>
             <hr className="rule" />
             <SocialIcons />
             <div className="button">
-              <Button className="btn">Source Code</Button>
-              <Button className="btn">Live Demo</Button>
+              <Button className="btn" onClick={handleClick}>
+                Source Code
+              </Button>
+              <Button className="btn" onClick={demoHandleClick}>
+                Live Demo
+              </Button>
             </div>
           </Card.Body>
         </Card>
--- src/pages/projects/data.js
+++ src/pages/projects/data.js
@@ -1,13 +1,15 @@
-export const data = [
+import myImages from "../../myImages/BudgetAppImg.png";
+
+const data = [
   {
     id: 1,
-    name: "The Tides At Arverne By The Sea",
-    image:
-      "https://photos.zillowstatic.com/fp/00ca20c0f040f67e66bfc6738275e5fa-cc_ft_1152.webp",
-    price: 3100.0,
-    location: "190 Beach 69th St, Arverne, NY 11692",
+    name: "My Budget App",
+    image: myImages,
+    react: "React",
+    ruby: "Ruby",
+    rails: "Rails",
     description:
-      "A new architectural masterpiece now dazzles the shores of the Rockaways with the addition of THE TIDES at Arverne By The Sea. Spectacular beachfront, affordable & luxurious apartments, offering modern open living spaces and high-end finishes.\n\n Amenities will include a roof deck with pool and lounge, indoor and outdoor dining experiences, private parking available for a fee, bike storage and more.",
+      "The project is about building a mobile web application where you can manage your budget: you have a list of transactions associated with a category, so that you can see how much money you spent and on what",
     github: "2023-04-07T14:45:25.938Z",
     linkedIn: "2023-04-07T14:45:25.938Z",
   },
