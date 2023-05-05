a0bfd3ffbc324740bf66069cf1a5a5e122d199b3
Alabi12 <wolo22001@gmail.com>
2023-05-05 01:17:29 +0000

    update the project file

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,16 +1,8 @@
 import React from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import data from "./data";
 import "./projects.css";
 import SocialIcons from "../../components/socialIcons/SocialIcons";
-
-const handleClick = () => {
-  window.open("https://github.com/Alabi12/Budget-App.git");
-};
-
-const demoHandleClick = () => {
-  window.open("https://budget-app-m3yc.onrender.com");
-};
 
 function Projects() {
   return (
@@ -29,12 +21,16 @@ function Projects() {
             <hr className="rule" />
             <SocialIcons />
             <div className="button">
-              <Button className="btn" onClick={handleClick}>
-                Source Code
-              </Button>
-              <Button className="btn" onClick={demoHandleClick}>
-                Live Demo
-              </Button>
+              <a href={data.demoLink} target="_blank" rel="noopener noreferrer">
+                Demo Link
+              </a>
+              <a
+                href={data.sourceLink}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Source Link
+              </a>
             </div>
           </Card.Body>
         </Card>
