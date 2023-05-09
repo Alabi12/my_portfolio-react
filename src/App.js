bc671b547a8b8b59868f2cf92de08024b37881d5
Alabi12 <wolo22001@gmail.com>
2023-05-09 10:09:08 +0000

    Add mobile version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import "./App.css";
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/home/Home";
-import Projects from "./pages/projects/Projects";
-import Abouts from "./pages/abouts/Abouts";
-import Contacts from "./pages/contacts/Contacts";
-import NavBar from "./components/navbar/NavBar";
+import Navbar from "./Components/Navbar";
+import About from "./Pages/About/About";
+import Contact from "./Pages/Contact/Contact";
+import Home from "./Pages/Home/Home";
+import Projects from "./Pages/Projects/Projects";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-          <NavBar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/project" element={<Projects />} />
-          <Route path="/about" element={<Abouts />} />
-          <Route path="/contact" element={<Contacts />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <div>
+      <Navbar />
+      <Home />
+      <About />
+      <Projects />
+      <Contact />
+    </div>
   );
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,24 +1,19 @@
 import React from "react";
-import "./home.css";
+import './home.css';
 
-function Home() {
+const Home = () => {
   return (
-    <>
-      <div className="splash-container">
-        <div className="main-page-text">
-          <h1>
-            I'm Robert <br /> Glad to see you!
-          </h1>
-          <p>
-            I'm a software developer! I can help you build a product , feature
-            or website Look through some of my work and experience! If you like
-            what you see and have a project you need coded, don't hestiate to
-            contact me.
-          </p>
-        </div>
-      </div>
-    </>
+    <div id="Home" className="splash-container" >
+      <h1>
+        I'm Robert <br /> Glad to see you!
+      </h1>
+      <p>
+        I'm a software developer! I can help you build a product , feature or
+        website Look through some of my work and experience! If you like what
+        you see and have a project you need coded, don't hestiate to contact me.
+      </p>
+    </div>
   );
-}
+};
 
 export default Home;
