3d6eb0b7dfed412f2eb40f84a98a35b8d5c19c8c
Alabi12 <wolo22001@gmail.com>
2023-05-09 19:05:41 +0000

    update the about.jsx

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import "./App.css";
-import About from "./pages/About/About";
-import Home from './pages/home/Home';
+import Home from "./pages/home/Home";
 import Navbar from "./components/Navbar";
-import Contacts from './pages/Contact/Contact'
+import Contacts from "./pages/Contact/Contact";
+import About from "./pages/About/About";
 import Projects from "./pages/projects/Projects";
-
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
       <Home />
       <About />
       <Projects />
       <Contacts />
-    </div>
+    </>
   );
 }
 
